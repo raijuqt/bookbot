ead18a3d2e0b6445e3b24ffede6dd82ac52db84c
raijuqt <raijuqt@gmail.com>
2025-06-23 18:27:18 +0100

    completed Bookbot

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from stats import get_num_words
+from stats import get_num_words, get_char_counts, sort_dict
+import sys
 
 
 def get_book_text(path_to_file):
@@ -6,4 +7,17 @@ def get_book_text(path_to_file):
         file_contents = f.read()
     return file_contents
 
-print(f"{get_num_words(get_book_text("books/frankenstein.txt"))} words found in the document")
+try: 
+    book = sys.argv[1]
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book}...")
+    print("----------- Word Count ----------")
+    print(f"Found {get_num_words(get_book_text(book))} total words")
+    print("--------- Character Count -------")
+    sorted_list = sort_dict(get_char_counts(get_book_text(book)))
+    for char in sorted_list:
+            print(f"{char["char"]}: {char["num"]}")
+    print("============= END ===============")
+except:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
--- stats.py
+++ stats.py
@@ -1,3 +1,29 @@
 def get_num_words(text):
     list_of_words = text.split()
     return len(list_of_words)
+
+def get_char_counts(text):
+    char_dict = {}
+    list_of_chars = list(text.lower())
+    
+    for char in list_of_chars:
+        if char not in char_dict:
+            char_dict[char] = 1
+        else:
+            char_dict[char] += 1
+    
+    return char_dict
+
+def sort_on(items):
+    return items["num"]
+
+def sort_dict(unsorted_dict):
+    sorted_char_list = []
+    
+    for char in unsorted_dict:
+        if char.isalpha():
+            sorted_char_list.append({"char": char, "num": unsorted_dict[char]})
+    
+    sorted_char_list.sort(reverse=True, key=sort_on)
+
+    return sorted_char_list
