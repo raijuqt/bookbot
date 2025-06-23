6823e74f5d96cc710a3d26a959ebf551ef088e65
raijuqt <raijuqt@gmail.com>
2025-06-23 17:22:41 +0100

    Ch2 L5

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("greetings boots")
+from stats import get_num_words
+
+
+def get_book_text(path_to_file):
+    with open(path_to_file) as f:
+        file_contents = f.read()
+    return file_contents
+
+print(f"{get_num_words(get_book_text("books/frankenstein.txt"))} words found in the document")
---
+++ stats.py
@@ -0,0 +1,3 @@
+def get_num_words(text):
+    list_of_words = text.split()
+    return len(list_of_words)
